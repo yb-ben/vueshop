7e88d1c63706880c50a9038395853e83918cf780
804141892@qq.com <804141892@qq.com>
2020-03-18 18:06:57 +0800

    save

--- src/utils/common.js
+++ src/utils/common.js
@@ -26,3 +26,38 @@ export function combine($arrs,str= '',pos = 0,){
     
     return combine($arrs,str,pos++)
 }
+
+
+export function foo($arr,$currHeight,$height,$obj,$attr, $save,$key,$cacheMap){
+    let t =  $arr[$currHeight];
+    let c = $currHeight +1;
+    for(let i = 0;i<t.length;i++){
+        if(!$currHeight){
+            $obj = {};
+            $key = '';
+        }
+        $obj['k'+c] = $attr[$currHeight].k;
+        $obj['k'+c+'_id'] = $attr[$currHeight].k_id; 
+        $obj['v'+c] = t[i].v;     
+        $obj['v'+c+'_id'] = t[i].v_id;
+        
+        //$key += 'k'+$attr[$currHeight].k_id + 'v'+t[i].v_id;
+
+        if(c < $height){
+            foo($arr,$currHeight+1,$height,$obj,$attr,$save,$key,$cacheMap);
+        }else{
+            let m ;
+            if($cacheMap.has($key)){
+                m = $cacheMap.get($key);
+                $obj.cast = m.cast;
+                $obj.weight = m.weight;
+                $obj.price = m.price;
+                $obj.code = m.code;
+                $cacheMap.delete($key);
+            }
+            let $new = Object.assign({},$obj);
+            $save.push($new);
+            $cacheMap.set($key,$new);
+        }
+    }
+}
--- src/views/spu/index.vue
+++ src/views/spu/index.vue
@@ -11,25 +11,44 @@
               filterable
               allow-create
             >
-              <el-option v-for="j in attrs" :key="j.k_id" :label="j.k" :value="j.k_id"></el-option>
+              <el-option
+                v-for="j in attrs"
+                :key="j.k_id"
+                :label="j.k"
+                :value="j.k_id"
+                :disabled="j.disabled"
+              ></el-option>
             </el-select>
             <el-button class="el-icon-close" @click="removeSpuItem(idx)"></el-button>
           </el-form-item>
-          <el-form-item label="规格值" >
-              <el-select v-for="(s,i) in a.values" :key="i"  filterable
-              allow-create @focus="onSpuValuesFocus(idx)">
-                <el-option v-for="(v) in getValues(idx)" :key="v.v_id" :label="v.v" :value="v.v_id"></el-option>
+
+          <el-form-item label="规格值">
+            <span v-for="(s,i) in a.values" :key="i">
+              <el-select
+                filterable
+                v-model="s.valueSelected"
+                allow-create
+                @change="onValuesSelected"
+              >
+                <el-option
+                  v-for="(vv) in a.allValues"
+                  :key="vv.v_id"
+                  :label="vv.v"
+                  :value="vv.v_id"
+                  :disabled="vv.disabled"
+                ></el-option>
               </el-select>
-              <el-button @click="addSpuValue">添加规格值</el-button>
+              <el-button class="el-icon-close" @click="removeSpuValue(idx,i)"></el-button>
+            </span>
+            <el-button v-if="a.selected" @click="addSpuValue(a)">添加规格值</el-button>
           </el-form-item>
         </el-row>
-
       </el-form>
 
       <el-button @click="addSpuItem" :disabled="addSpuItemBtnDisabled">添加规格项目</el-button>
     </el-form-item>
 
-    <el-form-item label="规格明细" label-width="120px">
+    <el-form-item label="规格明细" label-width="120px" v-if="spu.length>0">
       <el-table
         border
         :highlight-current-row="false"
@@ -40,13 +59,20 @@
         <el-table-column
           :prop="'v'+(i+1)"
           width="150"
-          v-for="(v,i) in attr"
+          v-for="(v,i) in getSpuAttr"
           :key="v.k_id"
           :label="v.k"
         ></el-table-column>
 
+  <el-table-column prop="price" label="价格" width="150" v-slot="scope">
+          <el-input v-model="scope.row.price"></el-input>
+        </el-table-column>
         <el-table-column prop="count" label="库存" width="150" v-slot="scope">
           <el-input v-model="scope.row.count"></el-input>
+        </el-table-column>
+
+           <el-table-column prop="code" label="规格编码" width="150" v-slot="scope">
+          <el-input v-model="scope.row.code"></el-input>
         </el-table-column>
 
         <el-table-column prop="cast" label="成本价" width="150" v-slot="scope">
@@ -59,186 +85,202 @@
 
 <script>
 import { attrs, addAttr, values, addValue } from "@/api/goods";
+import { foo } from "@/utils/common";
+
 export default {
   name: "SPUEditor",
   data() {
     return {
-      tableData: [],
       attrs: [],
       values: [],
       k1_id: "",
       k2_id: "",
       k3_id: "",
-      attr: [
-        
-      ],
-      spu: [
-        {
-          k1: "颜色",
-          k1_id: 1,
-          v1: "黑色",
-          v1_id: 100,
-          k2: "尺寸",
-          k2_id: 2,
-          v2: "大",
-          v2_id: 102,
-          count: 0,
-          cast: 0
-        },
-        {
-          k1: "颜色",
-          k1_id: 1,
-          v1: "黑色",
-          v1_id: 100,
-          k2: "尺寸",
-          k2_id: 2,
-          v2: "小",
-          v2_id: 103,
-          count: 0,
-          cast: 0
-        },
-        {
-          k1: "颜色",
-          k1_id: 1,
-          v1: "黄色",
-          v1_id: 101,
-          k2: "尺寸",
-          k2_id: 2,
-          v2: "大",
-          v2_id: 102,
-          count: 0,
-          cast: 0
-        },
-        {
-          k1: "颜色",
-          k1_id: 1,
-          v1: "黄色",
-          v1_id: 101,
-          k2: "尺寸",
-          k2_id: 2,
-          v2: "小",
-          v2_id: 103,
-          count: 0,
-          cast: 0
-        },
-        {
-          k1: "颜色",
-          k1_id: 1,
-          v1: "黑色",
-          v1_id: 100,
-          k2: "尺寸",
-          k2_id: 2,
-          v2: "大",
-          v2_id: 102,
-          count: 0,
-          cast: 0
-        },
-        {
-          k1: "颜色",
-          k1_id: 1,
-          v1: "黑色",
-          v1_id: 100,
-          k2: "尺寸",
-          k2_id: 2,
-          v2: "小",
-          v2_id: 103,
-          count: 0,
-          cast: 0
-        },
-        {
-          k1: "颜色",
-          k1_id: 1,
-          v1: "黄色",
-          v1_id: 101,
-          k2: "尺寸",
-          k2_id: 2,
-          v2: "大",
-          v2_id: 102,
-          count: 0,
-          cast: 0
-        },
-        {
-          k1: "颜色",
-          k1_id: 1,
-          v1: "黄色",
-          v1_id: 101,
-          k2: "尺寸",
-          k2_id: 2,
-          v2: "小",
-          v2_id: 103,
-          count: 0,
-          cast: 0
-        },
-        {
-          k1: "颜色",
-          k1_id: 1,
-          v1: "黑色",
-          v1_id: 100,
-          k2: "尺寸",
-          k2_id: 2,
-          v2: "大",
-          v2_id: 102,
-          count: 0,
-          cast: 0
-        },
-        {
-          k1: "颜色",
-          k1_id: 1,
-          v1: "黑色",
-          v1_id: 100,
-          k2: "尺寸",
-          k2_id: 2,
-          v2: "小",
-          v2_id: 103,
-          count: 0,
-          cast: 0
-        },
-        {
-          k1: "颜色",
-          k1_id: 1,
-          v1: "黄色",
-          v1_id: 101,
-          k2: "尺寸",
-          k2_id: 2,
-          v2: "大",
-          v2_id: 102,
-          count: 0,
-          cast: 0
-        },
-        {
-          k1: "颜色",
-          k1_id: 1,
-          v1: "黄色",
-          v1_id: 101,
-          k2: "尺寸",
-          k2_id: 2,
-          v2: "小",
-          v2_id: 103,
-          count: 0,
-          cast: 0
-        }
-      ],
+      attr: [],
+      // spu: [
+      //   // {
+      //   //   k1: "颜色",
+      //   //   k1_id: 1,
+      //   //   v1: "黑色",
+      //   //   v1_id: 100,
+      //   //   k2: "尺寸",
+      //   //   k2_id: 2,
+      //   //   v2: "大",
+      //   //   v2_id: 102,
+      //   //   count: 0,
+      //   //   cast: 0
+      //   // },
+      //   // {
+      //   //   k1: "颜色",
+      //   //   k1_id: 1,
+      //   //   v1: "黑色",
+      //   //   v1_id: 100,
+      //   //   k2: "尺寸",
+      //   //   k2_id: 2,
+      //   //   v2: "小",
+      //   //   v2_id: 103,
+      //   //   count: 0,
+      //   //   cast: 0
+      //   // },
+      //   // {
+      //   //   k1: "颜色",
+      //   //   k1_id: 1,
+      //   //   v1: "黄色",
+      //   //   v1_id: 101,
+      //   //   k2: "尺寸",
+      //   //   k2_id: 2,
+      //   //   v2: "大",
+      //   //   v2_id: 102,
+      //   //   count: 0,
+      //   //   cast: 0
+      //   // },
+      //   // {
+      //   //   k1: "颜色",
+      //   //   k1_id: 1,
+      //   //   v1: "黄色",
+      //   //   v1_id: 101,
+      //   //   k2: "尺寸",
+      //   //   k2_id: 2,
+      //   //   v2: "小",
+      //   //   v2_id: 103,
+      //   //   count: 0,
+      //   //   cast: 0
+      //   // },
+      //   // {
+      //   //   k1: "颜色",
+      //   //   k1_id: 1,
+      //   //   v1: "黑色",
+      //   //   v1_id: 100,
+      //   //   k2: "尺寸",
+      //   //   k2_id: 2,
+      //   //   v2: "大",
+      //   //   v2_id: 102,
+      //   //   count: 0,
+      //   //   cast: 0
+      //   // },
+      //   // {
+      //   //   k1: "颜色",
+      //   //   k1_id: 1,
+      //   //   v1: "黑色",
+      //   //   v1_id: 100,
+      //   //   k2: "尺寸",
+      //   //   k2_id: 2,
+      //   //   v2: "小",
+      //   //   v2_id: 103,
+      //   //   count: 0,
+      //   //   cast: 0
+      //   // },
+      //   // {
+      //   //   k1: "颜色",
+      //   //   k1_id: 1,
+      //   //   v1: "黄色",
+      //   //   v1_id: 101,
+      //   //   k2: "尺寸",
+      //   //   k2_id: 2,
+      //   //   v2: "大",
+      //   //   v2_id: 102,
+      //   //   count: 0,
+      //   //   cast: 0
+      //   // },
+      //   // {
+      //   //   k1: "颜色",
+      //   //   k1_id: 1,
+      //   //   v1: "黄色",
+      //   //   v1_id: 101,
+      //   //   k2: "尺寸",
+      //   //   k2_id: 2,
+      //   //   v2: "小",
+      //   //   v2_id: 103,
+      //   //   count: 0,
+      //   //   cast: 0
+      //   // },
+      //   // {
+      //   //   k1: "颜色",
+      //   //   k1_id: 1,
+      //   //   v1: "黑色",
+      //   //   v1_id: 100,
+      //   //   k2: "尺寸",
+      //   //   k2_id: 2,
+      //   //   v2: "大",
+      //   //   v2_id: 102,
+      //   //   count: 0,
+      //   //   cast: 0
+      //   // },
+      //   // {
+      //   //   k1: "颜色",
+      //   //   k1_id: 1,
+      //   //   v1: "黑色",
+      //   //   v1_id: 100,
+      //   //   k2: "尺寸",
+      //   //   k2_id: 2,
+      //   //   v2: "小",
+      //   //   v2_id: 103,
+      //   //   count: 0,
+      //   //   cast: 0
+      //   // },
+      //   // {
+      //   //   k1: "颜色",
+      //   //   k1_id: 1,
+      //   //   v1: "黄色",
+      //   //   v1_id: 101,
+      //   //   k2: "尺寸",
+      //   //   k2_id: 2,
+      //   //   v2: "大",
+      //   //   v2_id: 102,
+      //   //   count: 0,
+      //   //   cast: 0
+      //   // },
+      //   // {
+      //   //   k1: "颜色",
+      //   //   k1_id: 1,
+      //   //   v1: "黄色",
+      //   //   v1_id: 101,
+      //   //   k2: "尺寸",
+      //   //   k2_id: 2,
+      //   //   v2: "小",
+      //   //   v2_id: 103,
+      //   //   count: 0,
+      //   //   cast: 0
+      //   // }
+      // ],
+      spu:[],
+      vlenArr:[],
       maxSpuItem: 3,
-      maxSpuValue: 10
+      maxSpuValue: 10,
+      cacheMap:new Map(),
+      cnt: 0
     };
   },
 
-  watch: {
-    spu: {
-      deep: true,
-      handler() {
-        this.sendMsg();
-      }
-    }
-  },
+  watch: {},
 
   computed: {
     addSpuItemBtnDisabled() {
       return this.attr.length === this.maxSpuItem;
     },
 
-    getValues(idx){
-      return '';
+    getValues(idx) {
+      let s = this.attr[idx].selected;
+      let ret = null;
+      this.attrs.forEach((v, i) => {
+        if (v.k_id === s) ret = v.values;
+      });
+      return ret;
+    },
+
+    getSpuAttr() {
+      let t = this.spu[0];
+      let ret = [];
+      if (t.k1) {
+        ret.push({ k: t.k1, k_id: t.k1_id });
+      }
+      if (t.k2) {
+        ret.push({ k: t.k2, k_id: t.k2_id });
+      }
+      if (t.k3) {
+        ret.push({ k: t.k3, k_id: t.k3_id });
+      }
+      return ret;
     }
   },
 
@@ -251,65 +293,145 @@ export default {
     removeSpuItem(idx) {
       //移除规格项目
       //         this.spu.splice(idx,1);
+      let kid = this.attr[idx].k_id;
+      let t = this.attrs.find(x => {
+        return x.k_id === kid;
+      });
+      if (t) t.disabled = false;
       this.attr.splice(idx, 1);
+      this.getSpuArr();
     },
 
-    addSpuValue(idx, $v) {
+    addSpuValue(v) {
       //添加规格值
+      console.log(v);
+      v.values.push({});
+      this.$forceUpdate();
     },
 
-    removeSpuValue() {
+    removeSpuValue(pi, i) {
       //移除规格值
+      let v = this.attr[pi].values[i];
+
+      let t = this.attr[pi].allValues.find(x => {
+        return v.v_id === x.v_id;
+      });
+      if (t) t.disabled = false;
+      this.attr[pi].values.splice(i, 1);
+      this.$forceUpdate();
+      this.getSpuArr();
     },
 
-    onInputSpuItem(data) {
-      //输入新的规格项目
-      addAttr(data)
-        .then(resp => {})
-        .catch(err => {});
-    },
+    onValuesSelected(val) {
+      //选中规格值
+      let f = 0;
+      new Promise(resolve => {
+        if (typeof val !== "number") {
+          this.attr.forEach((v, i) => {
+            if (v.values) {
+              v.values.forEach((vv, ii) => {
+                if (vv.valueSelected === val) {
+                  let t = v.allValues.find(x => {
+                    return x.v_id === vv.v_id;
+                  });
+                  if (t) t.disabled = false;
 
-    onInputSpuValue(data) {
-      //输入新的规格值
-      addValue(data)
-        .then(resp => {})
-        .catch(err => {});
-    },
+                  addValue({ _id: v.k_id, val: val }).then(resp => {
+                    vv.v = val;
+                    vv.v_id = resp.data;
+                    v.allValues.push({
+                      v_id: resp.data,
+                      v: val,
+                      disabled: true
+                    });
 
-    onSpuValuesFocus(idx){
-      let v = this.getValues(idx);
-       
+                    this.$forceUpdate();
+                    resolve();
+                  });
+                  f = 1;
+                }
+              });
+              if (f) return false;
+            }
+          });
+        } else {
+          this.attr.forEach((v, i) => {
+            if (v.values) {
+              v.values.forEach((vv, ii) => {
+                if (vv.valueSelected === val) {
+                  // this.$set(vv, "valueSelected", val);
+                  let old_v_id = vv.v_id;
+                  v.allValues.forEach((vvv, iii) => {
+                    if (vvv.v_id === old_v_id) vvv.disabled = false;
+                    if (vvv.v_id === val) {
+                      vv.v = vvv.v;
+                      vvv.disabled = true;
+                    }
+                  });
+                  vv.v_id = val;
+                  this.$forceUpdate();
+                  f = 1;
+                  return false;
+                }
+              });
+            }
+            if (f) return false;
+          });
+          resolve();
+        }
+      }).then(resp => {
+        
+        this.getSpuArr();
+      });
     },
 
     onAttrSelected(val) {
-      console.log(val);
       //选中规格
-
-      if (typeof val !== "number") {
-        addAttr({ name: val })
-          .then(resp => {
+      console.log(val);
+      new Promise(resolve => {
+        if (typeof val !== "number") {
+          //添加新的属性值
+          addAttr({ name: val }).then(resp => {
             this.attr.forEach((v, i) => {
               if (v.selected === val) {
+                this.attrs.forEach((vv, ii) => {
+                  if (vv.k_id === v.k_id) vv.disabled = false;
+                });
                 v.selected = resp.data;
                 v.k = val;
                 v.k_id = resp.data;
+                v.allValues = [];
+                v.values = [{}];
               }
             });
-            this.attrs.push({ k_id: resp.data, k: val });
-          })
-          .catch(err => {});
-      } else {
-        this.attr.forEach((v, i) => {
-          if (v.selected === val) {
-            this.attrs.forEach((vv, ii) => {
-              if (vv.k_id === val) {
-                v.k = vv.k;
-                v.k_id = vv.k_id;
-              }
-            });
-          }
-        });
-      }
+            this.attrs.push({ k_id: resp.data, k: val, disabled: true });
+            resolve();
+          });
+        } else {
+          this.attr.forEach((v, i) => {
+            if (v.selected === val) {
+              let old_k_id = v.k_id;
+              this.attrs.forEach((vv, ii) => {
+                if (vv.k_id === old_k_id) vv.disabled = false;
+                if (vv.k_id === val) {
+                  vv.disabled = true;
+                  v.k = vv.k;
+                  v.k_id = vv.k_id;
+                  v.values = [{}];
+                  //获取属性值列表
+                  values({ _id: val }).then(resp => {
+                    this.$set(v, "allValues", resp.data);
+                    //v.allValues = resp.data;
+                  });
+                }
+              });
+            }
+          });
+          resolve();
+        }
+      }).then(resp => {
+        this.getSpuArr();
+      });
     },
 
     onAttrFocus() {
@@ -321,11 +443,69 @@ export default {
       }
     },
 
+    getSpuArr() {
+      //计算出SPU数组
+      console.log('start getSpuArr');
+      let ret = [];
+      let validAttrs = [];
+      let validValues = [];
+      let lenArr = [];
+      let success ;
+      let t ;
+     this.attr.forEach((v, i) => {
+       success = 0;
+       t = [];
+       if(v.k && v.k_id && v.values && v.values.length > 0 ){
+          v.values.forEach((vv,ii)=>{
+            if(vv.v && vv.v_id){
+              t.push(vv);
+              success++;
+            }
+          });
+          if(success){
+            v.pointer = t;
+            validAttrs.push(v);
+            //validValues.push(t);
+            lenArr.push(success);
+          }
+       }
+      });
+      
+      if(validAttrs.length>0){
+        validAttrs.sort((a,b)=>{
+          return a.k_id - b.k_id;
+        });
+        validAttrs.forEach((v)=>{
+          v.pointer.sort((a,b)=>{
+            return a.v_id - b.v_id;
+          })
+          validValues.push( v.pointer);
+        });
+
+        foo(validValues,0,validValues.length,{},validAttrs,ret,'',this.cacheMap);
+      }
+      this.vlenArr = lenArr;
+      this.spu = ret;
+    },
+
     arraySpanMethod({ row, column, rowIndex, columnIndex }) {
-      switch (this.attr.length) {
+      let len ;
+      if(this.spu.length ===0)return;
+      if(this.spu[0].k3){
+        len = 3;
+      }else if(this.spu[0].k2){
+        len = 2;
+      }else{
+        return;
+      }
+      
+      switch (len) {
         case 2:
           if (columnIndex === 0) {
-            let l = this.attr[1].v.length;
+
+
+            let l = this.vlenArr[1];
+            
             if (rowIndex % l === 0) {
               return [l, 1];
             } else {
@@ -334,15 +514,16 @@ export default {
           }
           break;
         case 3:
+          
           if (columnIndex === 1) {
-            let l = this.attr[2].v.length;
+            let l =this.vlenArr[2];
             if (rowIndex % l === 0) {
               return [l, 1];
             } else {
               return [0, 0];
             }
           } else if (columnIndex === 0) {
-            let l = this.attr[1].v.length * this.attr[2].v.length;
+            let l = this.vlenArr[1] * this.vlenArr[2];
             if (rowIndex % l === 0) {
               return [l, 1];
             } else {
